9b45ac6eaebe58fc122cb8e6a60f0c94196a7f2d
Yadpreet Randhawa <295484453+yadpreetrandhawa@users.noreply.github.com>
2026-09-04 19:04:43 -0700

    Initialize basic file structure

---
+++ particle.c
@@ -0,0 +1,13 @@
+#include "particle.h"
+
+void InitParticle(Particle *p, Vector2 position, float radius) {
+    return;
+}
+
+void UpdateParticle(Particle *p, float dt) {
+    return;
+}
+
+void DrawParticle(const Particle *p) {
+    return;
+}
---
+++ particle.h
@@ -0,0 +1,19 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include "raylib.h"
+
+// 2D partical struct
+typedef struct {
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 acceleration;
+    float radius;
+    Color color;
+} Particle;
+
+void InitParticle(Particle *p, Vector2 position, float radius);
+void UpdateParticle(Particle *p, float dt);
+void DrawParticle(const Particle *p);
+
+#endif
